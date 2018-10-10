d26cc165f0ceb1262ba3f520fda12e9733ffe5e1
Benjamin Bruno Meier <benjamin.meier70@gmail.com>
2018-10-10 22:56:49 +0200

    Implemented tests

--- lazy_load/__init__.py
+++ lazy_load/__init__.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 """lazy_load - A minimalistic interface that allows lazy evaluation of expressions / function calls / ..."""
 
-from ._lazy_load import lazy, lz, lazy_func, lf, ℒ, lazy_class, lz, force_eval, fe
+from ._lazy_load import lazy, lz, lazy_func, lf, ℒ, lazy_class, lc, force_eval, fe
 
 __version__ = '0.1.0'
 __author__ = 'Benjamin Bruno Meier <benjamin.meier70@gmail.com>'
-__all__ = [lazy, lz, lazy_func, lf, ℒ, lazy_class, lz, force_eval, fe]
 
+# __all__ is not used, because it is not possible to include there "ℒ":
+# AttributeError: module 'lazy_load' has no attribute 'ℒ'
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,4 +1,151 @@
-# Sample Test passing with nose and pytest
+from lazy_object_proxy import Proxy
+from lazy_load import *
 
-def test_pass():
-    assert True, "dummy sample test"
+
+def test_aliases():
+    assert lazy is lz
+
+    assert lazy_func is lf
+    assert lazy_func is ℒ
+
+    assert lazy_class is lc
+
+    assert force_eval is fe
+
+def test_lazy_simple_expression_evaluation():
+    evaluated = False
+    def add(x, y):
+        nonlocal evaluated
+        evaluated = True
+        return x + y
+    
+    res = lazy(lambda: add(1, 2))
+    assert not evaluated
+    assert type(res) is Proxy
+    assert res == 3
+    assert evaluated
+    assert isinstance(res, int)
+
+def test_lazy_function_with_eagle_argument_evaluation():
+    evaluated = False
+    def add(x, y):
+        nonlocal evaluated
+        evaluated = True
+        return x + y
+    
+    res = lazy(add, 1, 2)
+    assert not evaluated
+    assert type(res) is Proxy
+    assert res == 3
+    assert evaluated
+    assert isinstance(res, int)
+
+def test_lazy_function_evaluation():
+    evaluated = False
+    def add(x, y):
+        nonlocal evaluated
+        evaluated = True
+        return x + y
+    
+    lazy_add = ℒ[add] # this is equivalent to lazy_func(add)
+    res = lazy_add(1, 2)
+    assert not evaluated
+    assert type(res) is Proxy
+    assert res == 3
+    assert evaluated
+    assert isinstance(res, int)
+
+def test_lazy_function_decorator():
+    evaluated = False
+
+    @lazy_func
+    def lazy_add(x, y):
+        nonlocal evaluated
+        evaluated = True
+        return x + y
+    
+    res = lazy_add(1, 2)
+    assert not evaluated
+    assert type(res) is Proxy
+    assert res == 3
+    assert evaluated
+    assert isinstance(res, int)
+
+def test_lazy_class():
+    evaluated = False
+
+    @lazy_class
+    class LazyClass:
+        def __init__(self, n):
+            self._n = n
+        
+        def add(self, m) -> int:
+            nonlocal evaluated
+            evaluated = True
+            return self._n + m
+        
+        def sub(self, m) -> None: # Return type is None: This function should not be lazy evaluated
+            nonlocal evaluated
+            evaluated = True
+            return self._n - m
+        
+        @lazy_func
+        def mul(self, m) -> None: # Return type is None: This function should not be lazy evaluated; BUT there is the decorater: it makes the function lazy
+            nonlocal evaluated
+            evaluated = True
+            return self._n * m
+        
+        def _div(self, m) -> int: # Non-public functions are never made lazy by lazy_class
+            nonlocal evaluated
+            evaluated = True
+            return self._n // m
+    
+    lazy_obj = LazyClass(1)
+
+    res = lazy_obj.add(2)
+    assert not evaluated
+    assert type(res) is Proxy
+    assert res == 3
+    assert evaluated
+    assert isinstance(res, int)
+
+    evaluated = False
+    res = lazy_obj.sub(2)
+    assert type(res) is not Proxy
+    assert evaluated
+    assert res == -1
+
+    evaluated = False
+    res = lazy_obj.mul(2)
+    assert not evaluated
+    assert type(res) is Proxy
+    assert res == 2
+    assert evaluated
+    assert isinstance(res, int)
+
+    evaluated = False
+    res = lazy_obj._div(1)
+    assert type(res) is not Proxy
+    assert evaluated
+    assert res == 1
+    
+def test_force_eval_on_objects():
+    evaluated = False
+    def do_something(x, y):
+        nonlocal evaluated
+        evaluated = True
+    
+    res = lazy(do_something, 1, 2)
+    assert not evaluated
+    force_eval(res)
+    assert evaluated
+
+def test_force_eval_on_callables():
+    evaluated = False
+    def do_something():
+        nonlocal evaluated
+        evaluated = True
+    do_something_lazy = ℒ[do_something]
+    assert do_something_lazy is not do_something
+    assert fe(do_something_lazy) is do_something
+    assert not evaluated
